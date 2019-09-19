139047458948ff733ce8e96fb786ff21f6adbf8a
JeBermudez587 <jebermudez587@gmail.com>
2019-09-18 21:53:59 -0500

    Actualizacion 18/09/2019

--- plataforma/profesor/validar/subirpregunta.php
+++ plataforma/profesor/validar/subirpregunta.php
@@ -1,7 +1,27 @@
 <?php 
 include '../../conexion.php';
-$num = $_POST['num'];
 $examen = $_POST['examen'];
+$st = $conexion -> prepare("SELECT * FROM question WHERE examen='$examen'");
+$st -> execute();
+$contador = 1;
+while ($preguntas = $st -> fetch()) {
+	$contador++;
+}
+$num = $contador;
+if (isset($_POST['preguntadb'])) {
+	if ($_POST['preguntaa'] != "") {
+	$pregunta = $_POST['preguntaa'];
+	}else{
+		$pregunta = $_POST['preguntac'];
+	}
+	$idpdb = $_POST['preguntadb'];
+	$sq = $conexion -> prepare("UPDATE question SET pregunta='$pregunta' WHERE id='$idpdb'");
+	$sq -> execute();
+	$location = "location:../nuevo_examen.php?examen=".$examen."&num=".$num;
+echo $location;
+header($location);
+die();
+}
 if ($_POST['preguntaa'] != "") {
 	$pregunta = $_POST['preguntaa'];
 	echo $pregunta;
@@ -10,6 +30,12 @@ if ($_POST['preguntaa'] != "") {
 	$sq -> execute();
 }else{
 	$pregunta = $_POST['preguntac'];
+	if ($pregunta == "") {
+$location = "location:../nuevo_examen.php?examen=".$examen."&num=".$num;
+echo $location;
+header($location);
+die();
+	}
 	$correcta = $_POST['correcta'];
 	$adicional1 = $_POST['adicional1'];
 	$adicional2 = $_POST['adicional2'];
@@ -39,8 +65,9 @@ if ($_POST['preguntaa'] != "") {
 		$sq -> execute();
 	}	
 }
-$num = $num++;
-$location = "location:../nuevo_examen.php?num=".$num;
+$num = $num+1;
+$location = "location:../nuevo_examen.php?examen=".$examen."&num=".$num;
+echo $location;
 header($location);
 
 
